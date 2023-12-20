22401d94b315abb980da5d94a67f4a881cf29f63
Ernanni <ernannicf@gmail.com>
2023-12-20 03:44:40 -0300

    navigation with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Home } from './ui';
+import { Menu } from './features/menu';
+import { Cart } from './features/cart';
+import { CreateOrder, Order } from './features/order';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderID',
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
